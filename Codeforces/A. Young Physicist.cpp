a1d871363ba0c8ff7c12a00c368e770770d3b72b
Badrudduza Ahmed Fahim <41997813+bdafahim@users.noreply.github.com>
2020-05-13 19:40:30 +0600

    Add files via upload

---
+++ Codeforces/A. Young Physicist.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <queue>
+#include <map>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <limits.h>
+#include <bits/stdc++.h>
+#define fw(x) freopen("x.txt", "w", stdout)
+#define For(i, a, b, c) for (int i = a; i < b; i += c)
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REP1(i, n) for (int i = 1; i <= n; i++)
+#define mem(a, s) memset(a, s, sizeof a)
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL)
+#define pf printf
+#define sc scanf
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(v) v.begin(), v.end()
+#define vii vector<int>
+#define vll vector<ll>
+#define vss vector<string>
+#define debug(x) cout << #x "=" << (x) << endl
+#define pb push_back
+#define INF 1001001001
+#define PI 3.1415926535897932384626
+
+using namespace std;
+
+// moves
+
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*knight move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*knight move*/
+
+//big_mod
+
+//ll bigmod(ll a,ll b,ll m)
+//{if(b == 0) return 1%m;ll x = bigmod(a,b/2,m);x = (x * x) % m;if(b % 2 == 1) x = (x * a)% m;return x;}
+//ll BigMod(ll B,ll P,ll M){ ll R=1%M; while(P>0) {if(P%2==1){R=(R*B)%M;}P/=2;B=(B*B)%M;} return R;} /// (B^P)%M
+
+//ll getBit(ll num, int idx) {return ((num >> idx) & 1ll) == 1ll;}
+//ll setBit1(ll num, int idx) {return num or (1ll<<idx);}
+//ll setBit0(ll num, int idx) {return num & ~(1ll<<idx);}
+//ll flipBit(ll num, int idx) {return num ^ (1ll<<idx);}
+
+const int N = (int)2e5 + 5;
+const int mod = 1000000007;
+//ll qpow(ll n,ll k) {ll ans=1;assert(k>=0);n%=mod;while(k>0){if(k&1) ans=(ans*n)%mod;n=(n*n)%mod;k>>=1;}return ans%mod;}
+
+void printYes()
+{
+    cout<<"YES"<<"\n";
+}
+void printNo()
+{
+    cout<<"NO"<<"\n";
+}
+
+
+int main()
+{
+    int n, sz=3;
+    cin>>n;
+    vector<int>v1;
+    vector<int>v2;
+    vector<int>v3;
+    for(int i=0;i<n;i++)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+        v1.push_back(x);
+        v2.push_back(y);
+        v3.push_back(z);
+    }
+    int s1=0,s2=0,s3=0;
+
+    for(int i=0;i<v1.size();i++)
+    {
+        s1+=v1[i];
+    }
+    for(int i=0;i<v2.size();i++)
+    {
+        s2+=v2[i];
+    }
+    for(int i=0;i<v3.size();i++)
+    {
+        s3+=v3[i];
+    }
+
+    if(s1 ==0 && s2==0 && s3==0)
+    {
+        printYes();
+    }
+    else
+    {
+        printNo();
+    }
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
